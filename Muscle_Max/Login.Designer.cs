56ea6321070c167efd6f9afd22877019a4f7cb65
Yasitha Miyuranga <yasithamiyuranga219@gmail.com>
2025-12-24 23:32:59 +0530

    first commit

---
+++ Muscle_Max/Login.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Muscle_Max
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.UNameTb = new System.Windows.Forms.TextBox();
+            this.PasswordTb = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LoginBtn = new Bunifu.Framework.UI.BunifuThinButton2();
+            this.AdmLbl = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(296, 611);
+            this.panel1.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label8.Location = new System.Drawing.Point(32, 146);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(192, 31);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Fitness Center";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label7.Location = new System.Drawing.Point(46, 62);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(160, 31);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Gym Center";
+            // 
+            // UNameTb
+            // 
+            this.UNameTb.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UNameTb.Location = new System.Drawing.Point(202, 278);
+            this.UNameTb.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.UNameTb.Name = "UNameTb";
+            this.UNameTb.Size = new System.Drawing.Size(241, 29);
+            this.UNameTb.TabIndex = 3;
+           
+            // 
+            // PasswordTb
+            // 
+            this.PasswordTb.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PasswordTb.Location = new System.Drawing.Point(202, 395);
+            this.PasswordTb.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.PasswordTb.Name = "PasswordTb";
+            this.PasswordTb.Size = new System.Drawing.Size(241, 29);
+            this.PasswordTb.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(99, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(405, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Muscle Max GYM System";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(440, 228);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "User Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(433, 295);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 15;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(433, 295);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(204, 246);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "User Name";
+            // 
+            // LoginBtn
+            // 
+            this.LoginBtn.ActiveBorderThickness = 1;
+            this.LoginBtn.ActiveCornerRadius = 20;
+            this.LoginBtn.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.LoginBtn.ActiveForecolor = System.Drawing.Color.White;
+            this.LoginBtn.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.LoginBtn.BackColor = System.Drawing.SystemColors.Control;
+            this.LoginBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("LoginBtn.BackgroundImage")));
+            this.LoginBtn.ButtonText = "Login";
+            this.LoginBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LoginBtn.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoginBtn.ForeColor = System.Drawing.Color.SeaGreen;
+            this.LoginBtn.IdleBorderThickness = 1;
+            this.LoginBtn.IdleCornerRadius = 20;
+            this.LoginBtn.IdleFillColor = System.Drawing.Color.White;
+            this.LoginBtn.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.LoginBtn.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.LoginBtn.Location = new System.Drawing.Point(225, 462);
+            this.LoginBtn.Margin = new System.Windows.Forms.Padding(6);
+            this.LoginBtn.Name = "LoginBtn";
+            this.LoginBtn.Size = new System.Drawing.Size(180, 48);
+            this.LoginBtn.TabIndex = 9;
+            this.LoginBtn.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.LoginBtn.Click += new System.EventHandler(this.LoginBtn_Click);
+            // 
+            // AdmLbl
+            // 
+            this.AdmLbl.AutoSize = true;
+            this.AdmLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AdmLbl.Location = new System.Drawing.Point(272, 532);
+            this.AdmLbl.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.AdmLbl.Name = "AdmLbl";
+            this.AdmLbl.Size = new System.Drawing.Size(68, 25);
+            this.AdmLbl.TabIndex = 10;
+            this.AdmLbl.Text = "Admin";
+            this.AdmLbl.Click += new System.EventHandler(this.AdmLbl_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel2.BackgroundImage")));
+            this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel2.Controls.Add(this.label9);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.UNameTb);
+            this.panel2.Controls.Add(this.LoginBtn);
+            this.panel2.Controls.Add(this.PasswordTb);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.AdmLbl);
+            this.panel2.Location = new System.Drawing.Point(295, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(715, 611);
+            this.panel2.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(204, 360);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(92, 24);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Password";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1026, 611);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Text = "Login";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox UNameTb;
+        private System.Windows.Forms.TextBox PasswordTb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private Bunifu.Framework.UI.BunifuThinButton2 LoginBtn;
+        private System.Windows.Forms.Label AdmLbl;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ Muscle_Max/Login.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Muscle_Max
+{
+    public partial class Login : Form
+    {
+        Functions Con;
+        public Login()
+        {
+            InitializeComponent();
+            Con = new Functions();
+        }
+        public static int UserId;
+        private void AdmLbl_Click(object sender, EventArgs e)
+        {
+            Receptionists obj = new Receptionists();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+            if (UNameTb.Text == "" || PasswordTb.Text == "")
+            {
+                MessageBox.Show("Missing Information !!!");
+            }else
+            {
+                try
+                {
+                    String Query = "Select * from ReceptionistTbl where RecepName = '{0}' and RecepPass = '{1}'";
+                    Query = String.Format(Query, UNameTb.Text, PasswordTb.Text);
+                    DataTable dt = Con.GetData(Query);
+                    if(dt.Rows.Count == 0)
+                    {
+                        MessageBox.Show("Invalid Receptionist!!!");
+                    }else
+                    {
+                        UserId = Convert.ToInt32(dt.Rows[0][0].ToString());
+                        Members obj = new Members();
+                        obj.Show();
+                        this.Hide();
+
+                    }
+                }
+                catch(Exception Ex)
+                {
+                    MessageBox.Show(Ex.Message);
+                }
+            }
+        }
+
+        
+    }
+}
